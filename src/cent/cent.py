cd14837c0215bc8affb77002f97b1cd17dfb9809
Kayro César <39713656+kayrocesar@users.noreply.github.com>
2022-12-20 23:33:58 -0300

    remover função desnecessaria

--- src/cent/cent.py
+++ src/cent/cent.py
@@ -14,29 +14,6 @@ conns = {}
 addresses_list =[]
 
 
-def readFileConfigCent(name):
-  with open(name,'r') as file:
-    object = json.load(file)
-    conf = {}
-    conf['ip_servidor_central'] = object['ip_servidor_central']
-    conf['porta_servidor_central'] = object['porta_servidor_central']
-    conf['ip_servidor_distribuido'] = object['ip_servidor_distribuido']
-    conf['porta_servidor_distribuido'] = object['porta_servidor_distribuido']
-    conf['nome'] = object['nome']
-    conf['L_01'] = object['outputs'][0]['gpio']
-    conf['L_02'] = object['outputs'][1]['gpio']
-    conf['PR'] = object['outputs'][2]['gpio']
-    conf['AC'] = object['outputs'][3]['gpio']
-    conf['AL_BZ'] = object['outputs'][4]['gpio']
-    conf['SPres'] = object['inputs'][0]['gpio']
-    conf['SFum'] = object['inputs'][1]['gpio']
-    conf['SJan'] = object['inputs'][2]['gpio']
-    conf['SPor'] = object['inputs'][3]['gpio']
-    conf['SC_IN'] = object['inputs'][4]['gpio']
-    conf['SC_OUT'] = object['inputs'][5]['gpio']
-    conf['DHT22'] = object['sensor_temperatura'][0]['gpio']
-    return conf
-
 
 def sendRequest(conn, content):
     conn.send(content.encode('ascii'))
